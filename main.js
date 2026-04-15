86fc0bbca62d09b1cfc5df548a2098527638d05e
marciojoseconeglian <coneglian.marcio@escola.pr.gov.br>
2026-04-15 16:25:51 -0300

    1

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const botoes=document.querySelectorAll(".botao");
 console.log(botoes);
+
+for(let i=0; i<= botoes. length;i++){
+
+console.log(botoes[i]);
+botoes[i].onclick=function(){
+    botoes[i].classList.add("ativo");
+}
+
+}
